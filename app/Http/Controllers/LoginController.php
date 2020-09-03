8714bae4ed3bb24c3a02f887abab8e142ffa9b78
ashif <khan.ashif.ayaz@gmail.com>
2020-09-03 19:43:45 +0530

    ccc

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,21 +5,20 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        
-       $value='App\Services\Helper'::getRequest();
-      //  $new_value  = json_decode() ;
-       echo "<pre>"; 
+    /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+      
+      
+      return view('login');
+  }
 
-      print_r($value['Appli']);
-        die;   
-       
-        return view('login');
-    }
+ 
+
+
+
 }
