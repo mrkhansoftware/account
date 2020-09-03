885883d24e6175c113f04c6a8f850f4f713ac0e8
sunny-figma <sunnyfigma@gmail.com>
2020-09-03 16:54:48 +0530

    close account and change password

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('participantinformationaccount', 'ParticipantInformationAccountController');
+Route::resource('participantInformationAccount', 'ParticipantInformationAccountController');
 
 Route::get('/user', 'UserController@index');
 
-Route::get('/login', 'LoginController@index');
+Route::get('/login_account', 'LoginController@index');
+Route::post('/ajax_after_login', 'LoginController@ajaxAfterLogin');
+
 Route::get('/participant_information_account', 'ParticipantInformationAccountController@index');
 
-Route::get('/profile', 'ProfileController@index');
-Route::resource('profile_information', 'ProfileController');
+Route::get('/profile_account', 'ProfileController@index');
+
+Route::resource('profileInformation', 'ProfileController');
+
+Route::get('/change_password', 'ChangePasswordController@index');
+
+Route::resource('changePassword', 'ChangePasswordController');
+
+Route::get('/close_account', 'CloseAccountController@index');
+Route::resource('closeAccount', 'CloseAccountController');
 
 
 
