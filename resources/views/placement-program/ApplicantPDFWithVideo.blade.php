dcc16cee0e6171c363788bae7c9b513b3321bc48
ashif <khan.ashif.ayaz@gmail.com>
2020-10-13 12:19:46 +0530

    A

--- resources/views/placement-program/ApplicantPDFWithVideo.blade.php
+++ resources/views/placement-program/ApplicantPDFWithVideo.blade.php
@@ -2,7 +2,29 @@
 <html>
 
 <head>
+<title>Applicant Resume</title>
+<meta name="title" content="ApplicantPDFWithVideo" />
+    <meta name="description" content="Over the account you can access the programs and services" />
+    <meta name="keywords" content="Account IIEEX, Account GACCCA" />
+    <meta name="robots" content="noindex, nofollow" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="English" />
+    <meta name="revisit-after" content="30 days" />
+    <meta name="author" content="IIEEX, GACCCA" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://account.iieex.org/ApplicantPDFWithVideo" />
+    <meta property="og:site_name" content="https://account.iieex.org" />
+    <meta name="google-site-verification" content="-6V8H3sxoJ6qUsdyr6IkfGBlVoHhB5C3QQFTFTmR4mI" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="icon" href="{{ asset('images/favicon/favicon-iieex-16.png') }}" type="image/png" sizes="16x16" />
+    <link rel="icon" href="{{ asset('images/favicon/favicon-iieex-32.png') }}" type="image/png" sizes="32x32" />
+    <link rel="apple-touch-icon" href="{{ asset('images/favicon/favicon-iieex-60.png') }}" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('images/favicon/favicon-iieex-76.png') }}" />
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('images/favicon/favicon-iieex-120.png') }}" />
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('images/favicon/favicon-iieex-152.png') }}" />
+    
     <link rel="stylesheet" href="https://assets.ziggeo.com/v2-stable/ziggeo.css" />
     <link href="{{ asset('css/slds.css') }}" rel='stylesheet'>
     <link href="{{ asset('css/font.css') }}" rel='stylesheet'>
