9004996d2d3026d6a0e99873df7b7587e1ef76eb
ashif <khan.ashif.ayaz@gmail.com>
2020-10-16 17:34:49 +0530

    a

--- app/Http/Controllers/CaseInsensitiveUriValidator.php
+++ app/Http/Controllers/CaseInsensitiveUriValidator.php
@@ -2,12 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Route;
+use Illuminate\Routing\Route as IlluminateRoute;
 use Illuminate\Routing\Matching\ValidatorInterface;
 
 class CaseInsensitiveUriValidator implements ValidatorInterface
 {
   public function matches(Route $route, Request $request)
   {
+     $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+   if(stripos($actual_link,"www.localhost:8000")){
+    echo  $url=str_ireplace("www.","",$actual_link);
+    header("Location: ".$url);
+    exit();
+   }
     $path = $request->path() == '/' ? '/' : '/'.$request->path();
     $path=str_ireplace("/apex","",$path);//handling salesforce links
     return preg_match(preg_replace('/$/','i', $route->getCompiled()->getRegex()), rawurldecode($path));
