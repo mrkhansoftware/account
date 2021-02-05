10283c9ba01f5bbf0b521807fd930dea8d8bd650
ashif <khan.ashif.ayaz@gmail.com>
2021-02-05 14:09:31 +0530

    New

--- resources/views/placement-program/ApplicantPDFWithVideo.blade.php
+++ resources/views/placement-program/ApplicantPDFWithVideo.blade.php
@@ -177,7 +177,7 @@
 
     @elseif(isset($datas['app']['Template_design__c']) && $datas['app']['Template_design__c']=='Three')
 
-    @include('common.applicantcvresumetemplateThree',['datas' =>isset($datas)?$datas:''])
+    @include('common.ApplicantCVResumeTemplateThreeNew',['datas' =>isset($datas)?$datas:''])
 
     @else
     @endif
