6168c1bd4622c48a72901c49797fc5f88b37fe94
ashif <khan.ashif.ayaz@gmail.com>
2021-05-27 16:04:28 +0530

    Flooter

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -8,31 +8,38 @@
 jQuery(document).ready(function(){
 
 jQuery('.gaccca-input-date').datepicker({
-  dateFormat: "dd/mm/yy",
-  changeMonth:true,
-  changeYear:true,
-  yearRange: "-100:+50",
-  onClose: function () {
-        jQuery(this).parsley().validate();
-    }
+dateFormat: "dd/mm/yy",
+changeMonth:true,
+changeYear:true,
+yearRange: "-100:+50",
+onClose: function () {
+  jQuery(this).parsley().validate();
+}
 });
 
+jQuery('.gaccca-input-date').change(function() {
+var dateMMDDYYYRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+var daterequested=this.value;
+if(!daterequested.match(dateMMDDYYYRegex)){
+this.value='';
+}      
+});
 
 jQuery("#openNav").bind('click', function () {
-    jQuery("#mySidepanel").css('display', 'block');
-  });
+jQuery("#mySidepanel").css('display', 'block');
+});
 
-  jQuery("#closeNav").bind('click', function () {
-    jQuery("#mySidepanel").css('display', 'none');
-  })
+jQuery("#closeNav").bind('click', function () {
+jQuery("#mySidepanel").css('display', 'none');
+})
 
-  jQuery(document).ready(function () {
-    jQuery(window).resize(function () {
-      if (jQuery(window).width() >= 1093) {
-        jQuery("#mySidepanel").css('display', 'none');
-      }
-    });
-  });
+jQuery(document).ready(function () {
+jQuery(window).resize(function () {
+if (jQuery(window).width() >= 1093) {
+  jQuery("#mySidepanel").css('display', 'none');
+}
+});
+});
 
 
 });
@@ -40,112 +47,112 @@ jQuery("#openNav").bind('click', function () {
 var dropdown =  jQuery('.gaccca-dropdown-btn');
 var i;
 for (i = 0; i < dropdown.length; i++) {
-      dropdown[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var dropdownContent = this.nextElementSibling;
-      if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-      } else {
-      dropdownContent.style.display = "block";
-      }
-      });
- }
+dropdown[i].addEventListener("click", function() {
+this.classList.toggle("active");
+var dropdownContent = this.nextElementSibling;
+if (dropdownContent.style.display === "block") {
+dropdownContent.style.display = "none";
+} else {
+dropdownContent.style.display = "block";
+}
+});
+}
 
-  
+
 
 </script>
 
 <script language="javascript">
-  /* IE11 Fix for SP2010 */
-  if (typeof (UserAgentInfo) != 'undefined' && !window.addEventListener) {
-    UserAgentInfo.strBrowser = 1;
-  } 
+/* IE11 Fix for SP2010 */
+if (typeof (UserAgentInfo) != 'undefined' && !window.addEventListener) {
+UserAgentInfo.strBrowser = 1;
+} 
 </script>
 
 
 <script type="text/javascript">
- 
-  
-  //Get the button
 
-  var mybutton = jQuery("#gaccca-scroll-to-top-button")[0];
-  window.onscroll = function () { scrollFunction() };
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
+
+//Get the button
+
+var mybutton = jQuery("#gaccca-scroll-to-top-button")[0];
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+
+jQuery('#gaccca-scroll-to-top-button').click(function () {
+jQuery('html, body').animate({ scrollTop: 0 }, 'slow');
+});
+
+
+jQuery('input[type=file]').on("change", function (ev) {
+jQueryin = jQuery(this);
+var label_lab = jQuery(this).closest("label");
+var file_span = jQuery(label_lab).children("span");
+if (ev.originalEvent != null) {
+  if(this.files.length > 0){
+    var fileTypeAllowed=this.getAttribute('fileTypes')==null?'':this.getAttribute('fileTypes');
+    var uploadingFileType=this.files[0].type;
+    fileTypeAllowed=fileTypeAllowed.toLowerCase();
+    uploadingFileType=uploadingFileType.toLowerCase();
+
+    if(fileTypeAllowed!='' && !fileTypeAllowed.includes(uploadingFileType)){
+      this.value='';
+      alert('Please upload a valid file as mentioned.');return;
     }
+
+    
+  if(this.files[0].name.length > 35){
+    jQuery(file_span).text(this.files[0].name.slice(0, 25)+'...');
+  }else{
+    jQuery(file_span).text(this.files[0].name);
   }
 
-  jQuery('#gaccca-scroll-to-top-button').click(function () {
-    jQuery('html, body').animate({ scrollTop: 0 }, 'slow');
-  });
+  label_lab[0].title = this.files[0].name;
+  }
+}
+document.body.onfocus = function () {
+document.body.onfocus = null;
+setTimeout(function () {
+  if (jQueryin.val().length === 0) jQuery(file_span).text('Choose file...');
+}, 1000);
+};
+});
+function onUpdate(msg){  
+  jQuery('#message-success').html(msg);
+  jQuery('#success-alert').fadeIn();
+  setTimeout(function(){
+      
+      jQuery('#success-alert').fadeOut("slow");
+      
+  }, 5000);
+}
 
+function onError(msg){  
+  jQuery('#message-error').html(msg);
+  jQuery('#error-alert').fadeIn();
+  setTimeout(function(){
+      
+      jQuery('#error-alert').fadeOut("slow");
+      
+  }, 5000);
+}
+function showInput(value,conVal,section){
+document.getElementById(section).style.display=value==conVal?'block':'none';
+document.getElementById(section+'Field').required=value==conVal;
+}
 
-  jQuery('input[type=file]').on("change", function (ev) {
-    jQueryin = jQuery(this);
-    var label_lab = jQuery(this).closest("label");
-    var file_span = jQuery(label_lab).children("span");
-    if (ev.originalEvent != null) {
-       if(this.files.length > 0){
-         var fileTypeAllowed=this.getAttribute('fileTypes')==null?'':this.getAttribute('fileTypes');
-         var uploadingFileType=this.files[0].type;
-         fileTypeAllowed=fileTypeAllowed.toLowerCase();
-         uploadingFileType=uploadingFileType.toLowerCase();
-
-         if(fileTypeAllowed!='' && !fileTypeAllowed.includes(uploadingFileType)){
-           this.value='';
-           alert('Please upload a valid file as mentioned.');return;
-         }
-
-         
-        if(this.files[0].name.length > 35){
-          jQuery(file_span).text(this.files[0].name.slice(0, 25)+'...');
-        }else{
-          jQuery(file_span).text(this.files[0].name);
-        }
-
-        label_lab[0].title = this.files[0].name;
-       }
-    }
-    document.body.onfocus = function () {
-      document.body.onfocus = null;
-      setTimeout(function () {
-        if (jQueryin.val().length === 0) jQuery(file_span).text('Choose file...');
-      }, 1000);
-    };
-  });
-  function onUpdate(msg){  
-        jQuery('#message-success').html(msg);
-        jQuery('#success-alert').fadeIn();
-        setTimeout(function(){
-            
-            jQuery('#success-alert').fadeOut("slow");
-            
-        }, 5000);
-    }
-    
-    function onError(msg){  
-        jQuery('#message-error').html(msg);
-        jQuery('#error-alert').fadeIn();
-        setTimeout(function(){
-            
-            jQuery('#error-alert').fadeOut("slow");
-            
-        }, 5000);
-    }
-    function showInput(value,conVal,section){
-     document.getElementById(section).style.display=value==conVal?'block':'none';
-     document.getElementById(section+'Field').required=value==conVal;
-    }
-    
 
 function gacccaScriptContentTab(evt, btnId, tabName) {
 
 
 var i, gaccca_tabcontent, gaccc_tablinks;
- gaccca_tabcontent = jQuery(".gaccca-tabcontent");
+gaccca_tabcontent = jQuery(".gaccca-tabcontent");
 
 for (i = 0; i < gaccca_tabcontent.length; i++) {
 gaccca_tabcontent[i].style.display = "none";
@@ -167,42 +174,42 @@ gacccaScriptContentTab('','phase_btn_1', 'phase__1');
 
 
 jQuery("#openNav").bind('click',function(){
-  jQuery("#mySidepanel").css('display','block');
+jQuery("#mySidepanel").css('display','block');
 });
 
 jQuery("#closeNav").bind('click',function(){
-  jQuery("#mySidepanel").css('display','none');
+jQuery("#mySidepanel").css('display','none');
 })
 
 jQuery(document).ready(function(){
-  jQuery(window).resize(function(){
-   if (jQuery(window).width() >= 1093) {
-    jQuery("#mySidepanel").css('display','none');
+jQuery(window).resize(function(){
+if (jQuery(window).width() >= 1093) {
+jQuery("#mySidepanel").css('display','none');
 }
 
 
 
-  });
+});
 
-  
-  
+
+
 });
 
 
 
 
-    jQuery(document).ready(function() {
-      jQuery('[id^="gaccca_saveBtn_"]').click(function() {
-      jQuery('[id^="gacccaForm_"]').parsley().validate();
-      if (jQuery('[id^="gacccaForm_"]').parsley().isValid()) {
-              document.getElementById('loader').style.display='block';
-          } else {
-            document.getElementById('loader').style.display='none';
-          }
+jQuery(document).ready(function() {
+jQuery('[id^="gaccca_saveBtn_"]').click(function() {
+jQuery('[id^="gacccaForm_"]').parsley().validate();
+if (jQuery('[id^="gacccaForm_"]').parsley().isValid()) {
+        document.getElementById('loader').style.display='block';
+    } else {
+      document.getElementById('loader').style.display='none';
+    }
 
-    });
+});
 
-  });
+});
 
 
 
