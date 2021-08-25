410a3bb5927b9bf1453d5ed5dce038430407c4c3
ashif <khan.ashif.ayaz@gmail.com>
2021-08-25 13:33:25 +0530

    a

--- resources/views/common/scheduleCallComponent.blade.php
+++ resources/views/common/scheduleCallComponent.blade.php
@@ -224,7 +224,7 @@
         let slotBooked = '';
         let calLinks = '';
 
-        slotBooked = '<button class="gaccca-button-reshedule" id="rescheduleCallBtn">Reschdule your call</button>';
+        slotBooked = '<button class="gaccca-button-reshedule" id="rescheduleCallBtn">Reschedule your call</button>';
         slotBooked += '<p><strong>Call scheduled:</strong> ' + requestResponse.finalTimeSlotInfo + '</p>';
         slotBooked += '<p>Hangout Meet Room ID: <a target="_blank" href="https://' + requestResponse.hangOutLink + '">' + requestResponse.hangOutLink + '</a></p>';
 
