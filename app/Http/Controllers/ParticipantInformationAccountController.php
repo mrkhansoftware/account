6a0846908fc8c8160e4998a7a6413fcaccbe521a
ashif <khan.ashif.ayaz@gmail.com>
2021-10-04 15:19:26 +0530

    aa

--- app/Http/Controllers/ParticipantInformationAccountController.php
+++ app/Http/Controllers/ParticipantInformationAccountController.php
@@ -13,7 +13,7 @@ class ParticipantInformationAccountController extends Controller
      * @return \Illuminate\Http\Response
      */
     
-    public function index()    
+    public function index()      
     {
       $idCon= 'App\Services\Helper'::sessionConId();
       if($idCon==''){
